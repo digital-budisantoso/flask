cccd5beec67425bdd16c579e0eaa673690f8b00f
santosoaris <santosoaris@gmail.com>
2023-06-21 17:22:08 +0700

    adding menu api

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return jsonify({"Hallo": "Welcome to your Flask Application"})
 
+@app.route('/api/menu')
+def index():
+    return jsonify({"code": "1", "status": "OK", "menus":["menu1", "menu2", "menu3", "menu4"])	
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
